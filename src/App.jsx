589f695a98fbfa7d0d5375564e594078137e48a9
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-11-06 20:06:06 +0530

    highlight

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Hero,Sales } from './components';
-import { heroapi,popularsales, toprateslaes } from './data/data';
+import { FlexContent, Hero,Sales } from './components';
+import { heroapi,popularsales, toprateslaes, highlight, sneaker } from './data/data';
 
 const App = () => {
   return (
@@ -8,7 +8,9 @@ const App = () => {
     <main className='flex flex-col gap-16 relative'>
       <Hero heroapi={heroapi}/>
       <Sales endpoint={popularsales} ifExists />
+      <FlexContent endpoint={highlight} ifExists/>
       <Sales endpoint={toprateslaes}/>
+      <FlexContent endpoint={sneaker}/>
     </main>
     </>
   )
