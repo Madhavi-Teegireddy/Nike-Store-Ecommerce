79ede643e9d34eaec133f5d5228ba2d595dc8697
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-11-06 14:09:01 +0530

    popular-top-rated-sales

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Hero } from './components';
-import { heroapi } from './data/data';
+import { Hero,Sales } from './components';
+import { heroapi,popularsales, toprateslaes } from './data/data';
 
 const App = () => {
   return (
     <>
-    <main>
+    <main className='flex flex-col gap-16 relative'>
       <Hero heroapi={heroapi}/>
+      <Sales endpoint={popularsales} ifExists />
+      <Sales endpoint={toprateslaes}/>
     </main>
     </>
   )
